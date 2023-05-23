2e48e71d1fbb1dd2e233c03cccffe56223d74def
dlswn666 <dlswn666@gamil.com>
2023-05-23 17:37:39 +0900

    MainPage 이미지 마우스 오버시  음영 및 설명 추가

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,28 +4,46 @@ import '../css/MainPageStyle.css';
 const MainPage = () => {
   return (
     <div className="main-page">
-      <h2 className='title'>Main Page</h2>
+      <h2 className='title'>StoryIn</h2>
       <div className="image-container">
         <div className="row">
           <div className="image-wrapper">
             <img src="/images/introImage1.jpg" alt="Product  1" />
+            <div className="image-overlay">
+              <p className="image-text">Image 1 Description</p>
+            </div>
           </div>
           <div className="image-wrapper">
             <img src="/images/introImage2.jpg" alt="Product  2" />
+            <div className="image-overlay">
+              <p className="image-text">Image 2 Description</p>
+            </div>
           </div>
           <div className="image-wrapper">
             <img src="/images/introImage3.jpg" alt="Product  3" />
+            <div className="image-overlay">
+              <p className="image-text">Image 3 Description</p>
+            </div>
           </div>
         </div>
         <div className="row">
           <div className="image-wrapper">
             <img src="/images/introImage4.jpg" alt="Product  4" />
+            <div className="image-overlay">
+              <p className="image-text">Image 4 Description</p>
+            </div>
           </div>
           <div className="image-wrapper">
             <img src="/images/introImage5.jpg" alt="Product  5" />
+            <div className="image-overlay">
+              <p className="image-text">Image 5 Description</p>
+            </div>
           </div>
           <div className="image-wrapper">
             <img src="/images/introImage1.jpg" alt="Product  6" />
+            <div className="image-overlay">
+              <p className="image-text">Image 6 Description</p>
+            </div>
           </div>
         </div>
       </div>
