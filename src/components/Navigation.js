28febc9b98be385cf96520e9c27ca10f1d99f2d2
dlswn666 <dlswn666@gamil.com>
2023-05-25 09:51:28 +0900

    navi css 변경

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,26 +8,25 @@ const Navigation = () => {
             <div className='emtyContainer1'></div>
             <div className='content-container'>
                 <div className='headerTitle'>
-                    <h2 className='nav-item title'>StoryIn</h2>
+                    <Link to='/' className='nav-link'>
+                        <h2 className='nav-item title'>StoryIn</h2>
+                    </Link>
                 </div>
-                <ul className='nav-container-sub'>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-link'>
-                            Home
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/main' className='nav-link'>
-                            Main
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/about' className='nav-link'>
-                            About
-                        </Link>
-                    </li>
-                    {/* 다른 페이지에 대한 링크를 추가할 수 있습니다 */}
-                </ul>
+                <div className='nav-combine'>
+                    <ul className='nav-container-sub'>
+                        <li className='nav-item'>
+                            <Link to='/main' className='nav-link'>
+                                Main
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-link'>
+                                About
+                            </Link>
+                        </li>
+                        {/* 다른 페이지에 대한 링크를 추가할 수 있습니다 */}
+                    </ul>
+                </div>
             </div>
             <div className='emtyContainer2'></div>
         </nav>
